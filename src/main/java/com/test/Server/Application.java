f97e9c534fc75df619dec21a9a278014149e4c9a
Jack Fleitman <jfleit18@gmail.com>
2017-10-19 15:21:48 -0700

    Initial commit for just testing Spring. Uploaded to aws Elastic Beanstalk

---
+++ src/main/java/com/test/Server/Application.java
@@ -0,0 +1,18 @@
+package com.test.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication(scanBasePackages = "com.test")
+public class Application extends SpringBootServletInitializer{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
